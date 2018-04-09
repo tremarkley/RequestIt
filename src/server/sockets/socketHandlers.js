0fe93f57365e8003bba8bfe9a873e176d30aef93
Harry Markley <tremarkley@gmail.com>
2018-04-08 22:03:24 -0700

    Moved monitor current song into its own function

--- src/server/sockets/socketHandlers.js
+++ src/server/sockets/socketHandlers.js
@@ -3,37 +3,44 @@ const { getCurrentlyPlaying } = require('../spotifyAPIController/controller');
 
 const socketPort = process.env.SOCKET_PORT || 3332;
 
+const monitorCurrentSong = (client, interval) => {
+  let currentSong;
+  let waitingForResponse = false;
+  let newSongRequested = false;
+  //  set up monitoring interval
+  setInterval(() => {
+    if (!waitingForResponse) {
+      waitingForResponse = true;
+      getCurrentlyPlaying()
+        .then((data) => {
+          waitingForResponse = false;
+          //  song has changed
+          if (data.item.id !== currentSong) {
+            currentSong = data.item.id;
+            client.emit('newSong', data);
+          }
+          const msRemaining = data.item.duration - data.progress_ms;
+          //  put in request for new song if current song has less than 5 seconds remaing
+          if (msRemaining < 5000 && !newSongRequested) {
+            newSongRequested = true;
+            setTimeout(() => {
+              client.emit('requestNewSong', data);
+            }, msRemaining);
+          }
+          client.emit('currentlyPlaying', data);
+        })
+        .catch((error) => {
+          waitingForResponse = false;
+          console.log(`error getting currently playing ${error}`);
+        });
+    }
+  }, interval);
+};
+
 io.on('connect', (client) => {
   client.on('subscribeToCurrentlyPlaying', () => {
     console.log('client subscribing to currently playing song');
-    let currentSong;
-    let waitingForResponse = false;
-    let newSongRequested = false;
-    setInterval(() => {
-      if (!waitingForResponse) {
-        waitingForResponse = true;
-        getCurrentlyPlaying()
-          .then((data) => {
-            waitingForResponse = false;
-            if (data.item.id !== currentSong) {
-              currentSong = data.item.id;
-              client.emit('newSong', data);
-            }
-            const msRemaining = data.item.duration - data.progress_ms;
-            if (msRemaining < 5000 && !newSongRequested) {
-              newSongRequested = true;
-              setTimeout(() => {
-                client.emit('requestNewSong', data);
-              }, msRemaining);
-            }
-            client.emit('currentlyPlaying', data);
-          })
-          .catch((error) => {
-            waitingForResponse = false;
-            console.log(`error getting currently playing ${error}`);
-          });
-      }
-    }, 1000);
+    monitorCurrentSong(client, 1000);
   });
 });
 
